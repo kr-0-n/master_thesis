2c35246f08e04d18e758e0a1d358812f3f57f899
Lorenz Keefer <lorenz@keefer.de>
2024-12-02 12:03:45 +0100

    integration with ansible

--- k8_deployment/basic.tf
+++ k8_deployment/basic.tf
@@ -3,7 +3,11 @@ terraform {
   required_version = ">= 1.0"
   required_providers {
     openstack = {
-      source  = "terraform-provider-openstack/openstack"
+      source = "terraform-provider-openstack/openstack"
+    }
+    ansible = {
+      version = "~> 1.3.0"
+      source  = "ansible/ansible"
     }
   }
 }
@@ -14,15 +18,68 @@ terraform {
 provider "openstack" {}
 
 # Create a server
-resource "openstack_compute_instance_v2" "test-server" {
-  name = "test-server"
-  image_name = "GOLD Alma Linux 9"
-  flavor_name = "m1.small"
+resource "openstack_compute_instance_v2" "k8-worker" {
+  name        = "k8-worker-${count.index}"
+  count       = lookup(var.role_count, "worker")
+  image_name  = var.image
+  flavor_name = lookup(var.role_flavor, "worker")
 
-  key_pair = "lorenz_generic"
-  security_groups = [ "default", "SSH and ICMP" ]
+  key_pair        = "lorenz_generic"
+  security_groups = ["default", "SSH and ICMP"]
 
   network {
-    name = "IPv6"
+    name = var.network
+  }
+}
+
+resource "openstack_compute_instance_v2" "k8-manager" {
+  name        = "k8-manager-${count.index}"
+  count       = lookup(var.role_count, "manager")
+  image_name  = var.image
+  flavor_name = lookup(var.role_flavor, "manager")
+
+  key_pair        = "lorenz_generic"
+  security_groups = ["default", "SSH and ICMP"]
+
+  network {
+    name = var.network
+  }
+}
+
+
+resource "ansible_host" "k8-worker" {
+  count  = lookup(var.role_count, "worker", 0)
+  name   = "k8-worker-${count.index}"
+  groups = ["worker"] # Groups this host is part of
+
+  variables = {
+    ansible_host = trim(openstack_compute_instance_v2.k8-worker[count.index].access_ip_v4, "[]")
+  }
+}
+
+resource "ansible_host" "k8-manager" {
+  count  = lookup(var.role_count, "manager", 0)
+  name   = "k8-manager-${count.index}"
+  groups = ["manager"] # Groups this host is part of
+
+  variables = {
+    ansible_host = trim(openstack_compute_instance_v2.k8-manager[count.index].access_ip_v4, "[]")
+  }
+}
+
+# Ansible web group
+resource "ansible_group" "worker_group" {
+  name     = "worker_group"
+  children = ["worker"]
+  variables = {
+    ansible_user = "ubuntu"
+  }
+}
+
+resource "ansible_group" "manager_group" {
+  name     = "manager_group"
+  children = ["manager"]
+  variables = {
+    ansible_user = "ubuntu"
   }
 }
---
+++ k8_deployment/variables.tf
@@ -0,0 +1,38 @@
+variable "region" {
+    default = "osl"
+}
+
+variable "name" {
+  default = "k3s-test"
+}
+
+variable "ssh_public_key" {
+  default = "~/.ssh/uio-ifi-nd-msc-k8-nrec.pub"
+}
+
+variable "network" {
+  default = "dualStack"
+}
+
+# Mapping between role and image
+variable "image" {
+  default = "GOLD Ubuntu 20.04 LTS"
+}
+
+# Mapping between role and flavor
+variable "role_flavor" {
+  type = map(string)
+  default = {
+    "worker" = "m1.small"
+    "manager"  = "m1.large"
+  }
+}
+
+# Mapping between role and number of instances (count)
+variable "role_count" {
+  type = map(string)
+  default = {
+    "worker" = 2
+    "manager"  = 1
+  }
+}
