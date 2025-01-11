a14daca3bc00af2db59d4ab7c50de0ff682d0930
Lorenz Keefer <lorenz@keefer.de>
2025-01-11 14:19:23 +0100

    addded small testfile

--- simulator/test.py
+++ simulator/test.py
@@ -1,24 +1,12 @@
-import matplotlib.pyplot as plt
-import networkx as nx
+from visualizer import draw_graph
+import network_administration
+import conf
+from k8.kubernetes_wrapper import Kubernetes
+import metrics
 
-# Create an undirected graph
-G = nx.Graph()
+metrics.record_metrics = False
+k8 = Kubernetes(network_administration.setup_network(conf.graph), conf.algorithm)
+k8.deploy(conf.deployment)
+k8.tick()
 
-# Add nodes
-G.add_nodes_from([1, 2, 3, 4])
-
-# Add edges
-G.add_edges_from([(1, 2), (1, 3), (2, 4)])
-
-# Draw the graph
-plt.figure(figsize=(8, 6))
-nx.draw(G, with_labels=True, node_color='skyblue', node_size=1500, font_size=16, font_color='black', font_weight='bold')
-plt.title("Undirected Graph")
-plt.show()
-
-
-G.remove_node(1)
-
-nx.draw(G, with_labels=True, node_color='skyblue', node_size=1500, font_size=16, font_color='black', font_weight='bold')
-plt.title("Undirected Graph")
-plt.show()
+draw_graph(k8.graph, "k8: " )
