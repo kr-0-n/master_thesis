08a8b676d3a623db458cb9fce69e4163393674e6
Lorenz Keefer <lorenz@keefer.de>
2025-01-11 14:19:07 +0100

    refactored hardcoded network

--- simulator/network_administration.py
+++ simulator/network_administration.py
@@ -8,15 +8,17 @@ graph = None
 node_failures = {} # syntax: {node: [time_of_failure]}
 
 
-def setup_network():
+def setup_network(graph_dict):
     # Create an undirected graph
+    global shared_graph_dict
+    shared_graph_dict = graph_dict
     G = nx.Graph()
 
     # Add nodes
-    G.add_nodes_from(conf.simple_graph['nodes'])
+    G.add_nodes_from(shared_graph_dict['nodes'])
 
     # Add edges
-    G.add_edges_from(conf.simple_graph['edges'], type="connection")
+    G.add_edges_from(shared_graph_dict['edges'], type="connection")
     for edge in G.edges:
         G.edges[edge]["service"] = LinearFunction(G.edges[edge]["throughput"], 0, -G.edges[edge]["latency"])
         print(G.edges[edge])
@@ -27,7 +29,7 @@ def setup_network():
 
 def repair_network(graph: nx.Graph):
     # First check nodes
-    for node in conf.simple_graph['nodes']:
+    for node in shared_graph_dict['nodes']:
         if node[0] not in graph.nodes:
             print(f"{__name__}: Node {node[0]} offline")
             if not node[0] in node_failures: node_failures[node[0]] = []
@@ -35,7 +37,7 @@ def repair_network(graph: nx.Graph):
             graph.add_node(node[0], **node[1])
             print(f"{__name__}: Node {node[0]} back online")
     # Then check edges
-    for edge in conf.simple_graph['edges']:
+    for edge in shared_graph_dict['edges']:
         if (edge[0], edge[1]) not in graph.edges:
             print(f"{__name__}: Link ({edge[0]}, {edge[1]}) offline")
             graph.add_edge(edge[0], edge[1], type="connection", **edge[2], service=LinearFunction(edge[2]["throughput"], 0, -edge[2]["latency"]))
