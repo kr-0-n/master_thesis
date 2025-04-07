f548a284234306f93863c6196e0c43d0c35fa402
Lorenz Keefer <lorenz@keefer.de>
2025-04-07 08:37:48 +0200

    adjust for unconnected pods

--- simulator/k8/kubernetes_wrapper.py
+++ simulator/k8/kubernetes_wrapper.py
@@ -26,14 +26,14 @@ class Kubernetes:
         if self.current_deployment is not None:
             nodes_online_count = len([node for node in self.graph.nodes if self.graph.nodes[node]["type"] == "node"])
             update_metric('nodes_online', nodes_online_count)
-            pods_online_count = len([node for node in self.graph.nodes if self.graph.nodes[node]["type"] == "pod"])
+            pods_online_count = len([node for node in self.graph.nodes if self.graph.nodes[node]["type"] == "pod" and self.graph.neighbors(node) != []])
             update_metric('pods_online', pods_online_count)
             links_online_count = len([edge for edge in self.graph.edges if self.graph.edges[edge]["type"] == "connection"])
             update_metric('links_online', links_online_count)
 
 
             # check if all pods from the deployment are running
-            pods_to_schedule = [pod for pod in self.current_deployment['pods'] if pod[0] not in [node for node in self.graph.nodes if self.graph.nodes[node]["type"] == "pod"]] 
+            pods_to_schedule = [pod for pod in self.current_deployment['pods'] if pod[0] not in [node for node in self.graph.nodes if self.graph.nodes[node]["type"] == "pod"] or self.graph.neighbors(pod[0]) == []] 
             if len(pods_to_schedule) > 0:
                 print(f"{__name__}: Pod(s) {pods_to_schedule} are not running")
                 new_graph = self.scheduler.schedule(pods_to_schedule, self.graph)
