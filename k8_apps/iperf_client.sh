ab71c787b2f32727f0e80e4e29456db1c7dcc9ee
Lorenz Keefer <lorenz@keefer.de>
2025-04-13 09:42:11 +0200

    I should have not tested in production

--- k8_apps/iperf_client.sh
+++ k8_apps/iperf_client.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
+
 while true; do
-    iperf3 -c $TARGET --bandwidth $BANDWIDTH -p 5001 >> /logs/$HOSTNAME.log &
-    ping -c 10 $TARGET >> /logs/$HOSTNAME.log
+    # Start iperf3 in background
+    iperf3 -c "$TARGET" --bandwidth "$BANDWIDTH" -p 5001 >> /logs/"$HOSTNAME".log &
+    IPERF_PID=$!
+
+    # Start ping in background
+    ping -c 10 "$TARGET" >> /logs/"$HOSTNAME".log &
+    PING_PID=$!
+
+    # Wait for both to finish
+    wait $IPERF_PID
+    wait $PING_PID
+
+    # Sleep for 1-10 seconds
     sleep $((RANDOM % 10 + 1))
 done
