db8d9783bc9e6e7fa4de0f56f8c3715bab6fa156
Lorenz Keefer <lorenz@keefer.de>
2025-04-27 08:59:51 +0200

    added types and better node iteration

--- simulator/chaos_monkey.py
+++ simulator/chaos_monkey.py
@@ -1,4 +1,5 @@
 import networkx as nx
+from utils import *
 rnd =None
 """
 The chaos monkey creates chaos by deleting nodes, pods, and links. Beware, they are randomly chosen.
@@ -9,17 +10,12 @@ def delete_node(graph: nx.DiGraph):
     # remove random node and all adjacent assigned pods
     node = rnd.choice(list(node for node in graph.nodes if graph.nodes[node]["type"] == "node"))
     print(f"{__name__}: Deleting node {node}")
-    # adjacent_nodes = list(graph.neighbors(node))
-    # for adj_node in adjacent_nodes:
-    #     if graph.nodes[adj_node]["type"] == "pod":
-    #         graph.remove_node(adj_node)
     graph.remove_node(node)
-   
     return graph
 
-def delete_pod(graph):
+def delete_pod(graph: nx.DiGraph):
     global rnd
-    pods = [pod for pod in graph.nodes if graph.nodes[pod]["type"] == "pod"]
+    pods = get_pod_ids(graph)
 
     if pods:
         pod = rnd.choice(pods)
@@ -30,7 +26,7 @@ def delete_pod(graph):
         _ = rnd.random()
     return graph
 
-def delete_link(graph):
+def delete_link(graph: nx.DiGraph):
     global rnd
     available_links = list(link for link in graph.edges if "type" in graph.edges[link] and graph.edges[link]["type"] == "connection")
     # print(f"{__name__}: Available Connections: {available_links}")
