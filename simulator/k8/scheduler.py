50f4903ba4a0f6ee1fde427e11a306153d212fa3
Lorenz Keefer <lorenz@keefer.de>
2025-04-27 09:08:30 +0200

    added types

--- simulator/k8/scheduler.py
+++ simulator/k8/scheduler.py
@@ -6,7 +6,7 @@ import math
 class Scheduler():
     def __init__(self, algorithm):
         self.selected_algorithm = algorithm
-    def schedule(self, pods, graph):
+    def schedule(self, pods: list[dict], graph: nx.Graph) -> nx.Graph:
         # Remove and reschedule unassigned pods
         return self.selected_algorithm(graph, pods, debug=False, visualize=False)
     def optimize(self, graph):
