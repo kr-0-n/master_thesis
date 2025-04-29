7188017e20358c082c66a6000aa301586b0667c4
Lorenz Keefer <lorenz@keefer.de>
2025-04-29 23:37:03 +0200

    experiments

--- simulator/simulation.py
+++ simulator/simulation.py
@@ -7,39 +7,33 @@ import metrics
 import Time as time
 from utils import *
 
-rnd = Random()
-rnd.seed(conf.random_seed)
-chaos_monkey.rnd = rnd
 
-run_id = rnd.randint(1000, 9999)
+def run_simulation():
+    time.reset_time()
+    rnd = Random()
 
-metrics.initialize(conf.metrics_name_postfix, run_id)
-print("metrics initialized")
-k8 = kubernetes_wrapper.Kubernetes(network_administration.setup_network(conf.graph), conf.algorithm)
-k8.deploy(conf.deployment)
+    rnd.seed(conf.random_seed)
+    chaos_monkey.rnd = rnd
 
-while time.current_time_step() < 1440:
-    k8.tick()
-    if time.current_time_step() % 5 == 0:
-        print(f"network repaired at {time.current_time_step()}")
-        network_administration.repair_network(k8.graph)
-        # k8.scheduler.optimize(k8.graph)
+    run_id = rnd.randint(1000, 9999)
 
-    # randomely invoke the chaos monkey
-    if rnd.random() < conf.node_delete_probability:
-        print(f"chaos monkey invoked at {time.current_time_step()}")
-        if len(get_node_ids(k8.graph)) > 1:
-            chaos_monkey.delete_node(k8.graph)
-            pass
-    if rnd.random() < conf.pod_delete_probability:
-        print(f"chaos monkey invoked at {time.current_time_step()}")
-        chaos_monkey.delete_pod(k8.graph)
-        pass
-    if rnd.random() < conf.link_delete_probability:
-        print(f"chaos monkey invoked at {time.current_time_step()}")
-        chaos_monkey.delete_link(k8.graph)
-    # periodically repair the network
+    metrics.initialize(conf.metrics_name_postfix, run_id)
+    print("metrics initialized")
+    k8 = kubernetes_wrapper.Kubernetes(network_administration.setup_network(conf.graph), conf.algorithm)
+    k8.deploy(conf.deployment)
 
-    time.increment_time()
-    print(f"{__name__}: Time: {time.current_time_step()}")
-print(f"Simulation k8_simulation_{run_id}_{conf.metrics_name_postfix} finished at {time.current_time_step()}")
+    while time.current_time_step() < 1440:
+        k8.tick()
+        if time.current_time_step() % 5 == 0:
+            print(f"network repaired at {time.current_time_step()}")
+            network_administration.repair_network(k8.graph)
+            # k8.scheduler.optimize(k8.graph)
+
+        # invoke the chaos monkey
+        chaos_monkey.delete_nodes(k8.graph)
+        chaos_monkey.delete_links(k8.graph)
+        chaos_monkey.delete_pods(k8.graph)
+
+        time.increment_time()
+        print(f"{__name__}: Time: {time.current_time_step()}")
+    print(f"Simulation k8_simulation_{run_id}_{conf.metrics_name_postfix} finished at {time.current_time_step()}")
