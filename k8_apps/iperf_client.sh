5eeccc34430b7763a7c0a237feceb762af854003
Lorenz Keefer <lorenz@keefer.de>
2026-03-17 09:37:31 +0100

    Better Iperf bandwidth control

--- k8_apps/iperf_client.sh
+++ k8_apps/iperf_client.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 while true; do
-    echo "TIME: $(date +%s)" >> /logs/"$HOSTNAME".log
-    # Try iperf3 on a range of ports and run the first one that works
-    for port in $(seq 5002 5012); do
-        iperf3 -c "$TARGET" --bandwidth "$BANDWIDTH" -p "$port" >> /logs/"$HOSTNAME".log 
-        if [[ "$?" -eq 0 ]]; then
-          break 
-        fi
+  echo "TIME: $(date +%s)" >>/logs/"$HOSTNAME".log
+  # Try iperf3 on a range of ports and run the first one that works
+  for port in $(seq 5002 5012); do
+    iperf3 -c "$TARGET" -u --bandwidth "$BANDWIDTH" -l 300 -p "$port" --pacing-timer 1000 >>/logs/"$HOSTNAME".log
+    if [[ "$?" -eq 0 ]]; then
+      break
+    fi
 
-    done
+  done
 
-    # Start hping3 in TCP mode in background
-    hping3 --syn -p 5001 -c 10 "$TARGET" >> /logs/"$HOSTNAME".log 
+  # Start hping3 in TCP mode in background
+  hping3 --syn -p 5001 -c 10 "$TARGET" >>/logs/"$HOSTNAME".log
 
-    # Sleep for 1-10 seconds
-    sleep $((RANDOM % 10 + 1))
+  # Sleep for 1-10 seconds
+  sleep $((RANDOM % 10 + 1))
 done
