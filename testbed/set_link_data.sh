c4d6fddfd16a1f3d6c37b3445ca5586bb23019ef
Lorenz Keefer <lorenz@keefer.de>
2026-05-29 10:48:55 +0200

    Link setter

--- testbed/set_link_data.sh
+++ testbed/set_link_data.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 # Tetra Cell 1
-grpcurl -plaintext -d '{"from": "multinode", "to": "multinode-m08", "latency": 5, "throughput": 0.5}' -import-path /home/kron/projects/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
-grpcurl -plaintext -d '{"from": "multinode-m08", "to": "multinode", "latency": 5, "throughput": 0.5}' -import-path /home/kron/projects/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
-grpcurl -plaintext -d '{"from": "multinode", "to": "multinode-m07", "latency": 5, "throughput": 0.5}' -import-path /home/kron/projects/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
-grpcurl -plaintext -d '{"from": "multinode-m07", "to": "multinode", "latency": 5, "throughput": 0.5}' -import-path /home/kron/projects/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
-grpcurl -plaintext -d '{"from": "multinode", "to": "multinode-m06", "latency": 5, "throughput": 0.5}' -import-path /home/kron/projects/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
-grpcurl -plaintext -d '{"from": "multinode-m06", "to": "multinode", "latency": 5, "throughput": 0.5}' -import-path /home/kron/projects/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
-grpcurl -plaintext -d '{"from": "multinode-m07", "to": "multinode-m06", "latency": 5, "throughput": 0.5}' -import-path /home/kron/projects/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
-grpcurl -plaintext -d '{"from": "multinode-m06", "to": "multinode-m07", "latency": 5, "throughput": 0.5}' -import-path /home/kron/projects/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
-grpcurl -plaintext -d '{"from": "multinode-m08", "to": "multinode-m06", "latency": 5, "throughput": 0.5}' -import-path /home/kron/projects/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
-grpcurl -plaintext -d '{"from": "multinode-m06", "to": "multinode-m08", "latency": 5, "throughput": 0.5}' -import-path /home/kron/projects/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
-grpcurl -plaintext -d '{"from": "multinode-m08", "to": "multinode-m07", "latency": 5, "throughput": 0.5}' -import-path /home/kron/projects/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
-grpcurl -plaintext -d '{"from": "multinode-m07", "to": "multinode-m08", "latency": 5, "throughput": 0.5}' -import-path /home/kron/projects/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
+grpcurl -plaintext -d '{"from": "minikube", "to": "minikube-m08", "latency": 5, "throughput": 0.5}' -import-path $HOME/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
+grpcurl -plaintext -d '{"from": "minikube-m08", "to": "minikube", "latency": 5, "throughput": 0.5}' -import-path $HOME/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
+grpcurl -plaintext -d '{"from": "minikube", "to": "minikube-m07", "latency": 5, "throughput": 0.5}' -import-path $HOME/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
+grpcurl -plaintext -d '{"from": "minikube-m07", "to": "minikube", "latency": 5, "throughput": 0.5}' -import-path $HOME/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
+grpcurl -plaintext -d '{"from": "minikube", "to": "minikube-m06", "latency": 5, "throughput": 0.5}' -import-path $HOME/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
+grpcurl -plaintext -d '{"from": "minikube-m06", "to": "minikube", "latency": 5, "throughput": 0.5}' -import-path $HOME/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
+grpcurl -plaintext -d '{"from": "minikube-m07", "to": "minikube-m06", "latency": 5, "throughput": 0.5}' -import-path $HOME/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
+grpcurl -plaintext -d '{"from": "minikube-m06", "to": "minikube-m07", "latency": 5, "throughput": 0.5}' -import-path $HOME/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
+grpcurl -plaintext -d '{"from": "minikube-m08", "to": "minikube-m06", "latency": 5, "throughput": 0.5}' -import-path $HOME/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
+grpcurl -plaintext -d '{"from": "minikube-m06", "to": "minikube-m08", "latency": 5, "throughput": 0.5}' -import-path $HOME/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
+grpcurl -plaintext -d '{"from": "minikube-m08", "to": "minikube-m07", "latency": 5, "throughput": 0.5}' -import-path $HOME/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
+grpcurl -plaintext -d '{"from": "minikube-m07", "to": "minikube-m08", "latency": 5, "throughput": 0.5}' -import-path $HOME/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
 
 # Tetra Cell 2
-grpcurl -plaintext -d '{"from": "multinode-m03", "to": "multinode-m02", "latency": 10, "throughput": 0.5}' -import-path /home/kron/projects/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
-grpcurl -plaintext -d '{"from": "multinode-m02", "to": "multinode-m03", "latency": 10, "throughput": 0.5}' -import-path /home/kron/projects/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
-grpcurl -plaintext -d '{"from": "multinode-m02", "to": "multinode-m05", "latency": 20, "throughput": 0.5}' -import-path /home/kron/projects/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
-grpcurl -plaintext -d '{"from": "multinode-m05", "to": "multinode-m02", "latency": 20, "throughput": 0.5}' -import-path /home/kron/projects/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
-grpcurl -plaintext -d '{"from": "multinode-m05", "to": "multinode-m04", "latency": 20, "throughput": 0.5}' -import-path /home/kron/projects/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
-grpcurl -plaintext -d '{"from": "multinode-m04", "to": "multinode-m05", "latency": 20, "throughput": 0.5}' -import-path /home/kron/projects/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
-grpcurl -plaintext -d '{"from": "multinode-m03", "to": "multinode-m04", "latency": 10, "throughput": 0.5}' -import-path /home/kron/projects/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
-grpcurl -plaintext -d '{"from": "multinode-m04", "to": "multinode-m03", "latency": 10, "throughput": 0.5}' -import-path /home/kron/projects/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
-grpcurl -plaintext -d '{"from": "multinode-m03", "to": "multinode-m05", "latency": 20, "throughput": 0.5}' -import-path /home/kron/projects/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
-grpcurl -plaintext -d '{"from": "multinode-m05", "to": "multinode-m03", "latency": 20, "throughput": 0.5}' -import-path /home/kron/projects/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
-grpcurl -plaintext -d '{"from": "multinode-m04", "to": "multinode-m02", "latency": 10, "throughput": 0.5}' -import-path /home/kron/projects/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
-grpcurl -plaintext -d '{"from": "multinode-m02", "to": "multinode-m04", "latency": 10, "throughput": 0.5}' -import-path /home/kron/projects/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
+grpcurl -plaintext -d '{"from": "minikube-m03", "to": "minikube-m02", "latency": 10, "throughput": 0.08}' -import-path $HOME/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
+grpcurl -plaintext -d '{"from": "minikube-m02", "to": "minikube-m03", "latency": 10, "throughput": 0.08}' -import-path $HOME/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
+grpcurl -plaintext -d '{"from": "minikube-m02", "to": "minikube-m05", "latency": 10, "throughput": 0.08}' -import-path $HOME/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
+grpcurl -plaintext -d '{"from": "minikube-m05", "to": "minikube-m02", "latency": 10, "throughput": 0.08}' -import-path $HOME/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
+grpcurl -plaintext -d '{"from": "minikube-m05", "to": "minikube-m04", "latency": 20, "throughput": 0.08}' -import-path $HOME/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
+grpcurl -plaintext -d '{"from": "minikube-m04", "to": "minikube-m05", "latency": 20, "throughput": 0.08}' -import-path $HOME/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
+grpcurl -plaintext -d '{"from": "minikube-m03", "to": "minikube-m04", "latency": 20, "throughput": 0.08}' -import-path $HOME/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
+grpcurl -plaintext -d '{"from": "minikube-m04", "to": "minikube-m03", "latency": 20, "throughput": 0.08}' -import-path $HOME/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
+grpcurl -plaintext -d '{"from": "minikube-m03", "to": "minikube-m05", "latency": 10, "throughput": 0.08}' -import-path $HOME/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
+grpcurl -plaintext -d '{"from": "minikube-m05", "to": "minikube-m03", "latency": 10, "throughput": 0.08}' -import-path $HOME/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
+grpcurl -plaintext -d '{"from": "minikube-m04", "to": "minikube-m02", "latency": 20, "throughput": 0.08}' -import-path $HOME/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
+grpcurl -plaintext -d '{"from": "minikube-m02", "to": "minikube-m04", "latency": 20, "throughput": 0.08}' -import-path $HOME/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
 
 # Stacom
-grpcurl -plaintext -d '{"from": "multinode-m08", "to": "multinode-m02", "latency": 250, "throughput": 0.55}' -import-path /home/kron/projects/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
-grpcurl -plaintext -d '{"from": "multinode-m02", "to": "multinode-m08", "latency": 250, "throughput": 0.55}' -import-path /home/kron/projects/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
+grpcurl -plaintext -d '{"from": "minikube-m08", "to": "minikube-m05", "latency": 250, "throughput": 0.55}' -import-path $HOME/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
+grpcurl -plaintext -d '{"from": "minikube-m05", "to": "minikube-m08", "latency": 250, "throughput": 0.55}' -import-path $HOME/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
+
+# High Speed Link
+# grpcurl -plaintext -d '{"from": "minikube", "to": "minikube-m02", "latency": 2, "throughput": 100}' -import-path $HOME/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
+# grpcurl -plaintext -d '{"from": "minikube-m02", "to": "minikube", "latency": 2, "throughput": 100}' -import-path $HOME/master_thesis/api_server/proto -proto message.proto 127.0.0.1:50051 links.LinkService/SendData
